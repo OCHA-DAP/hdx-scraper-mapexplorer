16e06b4c25fd5d9b47a353584fb2bda87090ccfe
Mike <rans@email.com>
2018-02-23 14:59:16 +0100

    Add user agent

--- run.py
+++ run.py
@@ -47,5 +47,5 @@ def main():
 
 
 if __name__ == '__main__':
-    facade(main, hdx_site='test', project_config_yaml=join('config', 'project_configuration.yml'))
+    facade(main, hdx_site='test', user_agent_config_yaml=join(expanduser('~'), '.mapexploreruseragent.yml'), project_config_yaml=join('config', 'project_configuration.yml'))
 
